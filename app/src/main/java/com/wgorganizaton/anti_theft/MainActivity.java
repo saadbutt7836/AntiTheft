f3b1cd26415fba78f63f8461fe27616be05d2fea
saadbutt7836 <saadbutt7836@gmail.com>
2020-06-09 11:36:16 -0700

    Initial commit

---
+++ app/src/main/java/com/wgorganizaton/anti_theft/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wgorganizaton.anti_theft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
